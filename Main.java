c686ad9f592104bc81ba4002ee841b0f8b995a80
Kirill <kiruxastar@gmail.com>
2020-08-21 11:29:52 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 2_000_000_000;
+        int Transfer = 500_000_000;
+        int TotalSum = count + Transfer;
+        System.out.println(TotalSum);
+    }
+}
